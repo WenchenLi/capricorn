0e8a116dc2edfb43f439fa2030aa5e7054394cb0
wenchenli <wenchen.li.cs@gmail.com>
2018-12-07 19:19:57 +0800

    fix code format

--- nlp_vocab/Vocab.py
+++ nlp_vocab/Vocab.py
@@ -37,117 +37,117 @@ PAD_TOKEN = '__PAD__'
 
 
 class Vocabulary(object):
-	"""
-	Categorical variables vocabulary class.
-	Accumulates and provides mapping from classes to indexes.
-	Can be easily used for words.
-	"""
+  """
+  Categorical variables vocabulary class.
+  Accumulates and provides mapping from classes to indexes.
+  Can be easily used for words.
+  """
 
-	def __init__(self, addtional_predefined_tokens_dict=None, support_reverse=True):
-		self.UNK_id = 0
-		self._unknown_token = UNKNOWN_TOKEN
-		self._mapping = {UNKNOWN_TOKEN: self.UNK_id,PAD_TOKEN:1}
-		if addtional_predefined_tokens_dict:
-			for k in addtional_predefined_tokens_dict:
-				self._mapping[k] = self._mapping.__len__()
-		self._support_reverse = support_reverse
-		if support_reverse:
-			self._reverse_mapping = [item[0] for item in sorted(self._mapping.items(), key=lambda x:x[1])]
-		self._freq = collections.defaultdict(int)
-		self._freeze = False
+  def __init__(self, addtional_predefined_tokens_dict=None, support_reverse=True):
+    self.UNK_id = 0
+    self._unknown_token = UNKNOWN_TOKEN
+    self._mapping = {UNKNOWN_TOKEN: self.UNK_id, PAD_TOKEN: 1}
+    if addtional_predefined_tokens_dict:
+      for k in addtional_predefined_tokens_dict:
+        self._mapping[k] = self._mapping.__len__()
+    self._support_reverse = support_reverse
+    if support_reverse:
+      self._reverse_mapping = [item[0] for item in sorted(self._mapping.items(), key=lambda x: x[1])]
+    self._freq = collections.defaultdict(int)
+    self._freeze = False
 
-	def __len__(self):
-		"""Returns total count of mappings. Including unknown token."""
-		return len(self._mapping)
+  def __len__(self):
+    """Returns total count of mappings. Including unknown token."""
+    return len(self._mapping)
 
-	def freeze(self, freeze=True):
-		"""Freezes the vocabulary, after which for new words return unknown token id in self.get.
-				freeze helps on decide actions when use self.get and self.load_embedding
-		Args:
-			freeze: True to freeze, False to unfreeze.
-		"""
-		self._freeze = freeze
+  def freeze(self, freeze=True):
+    """Freezes the vocabulary, after which for new words return unknown token id in self.get.
+        freeze helps on decide actions when use self.get and self.load_embedding
+    Args:
+      freeze: True to freeze, False to unfreeze.
+    """
+    self._freeze = freeze
 
-	def get_freeze_state(self):
-		"""getter for freeze state"""
-		return self._freeze
+  def get_freeze_state(self):
+    """getter for freeze state"""
+    return self._freeze
 
-	def get(self, category):
-		"""
-		Returns word's id in the vocabulary. If category is new, creates a new id for it.
+  def get(self, category):
+    """
+    Returns word's id in the vocabulary. If category is new, creates a new id for it.
 
-		Args:
-			category: string or integer to lookup in vocabulary.
+    Args:
+      category: string or integer to lookup in vocabulary.
 
-		Returns:
-			interger, id in the vocabulary.
-		"""
-		if category not in self._mapping:
-			if self._freeze:
-				return self.UNK_id
-			self._mapping[category] = len(self._mapping)
-			if self._support_reverse:
-				self._reverse_mapping.append(category)
-		return self._mapping[category]
+    Returns:
+      interger, id in the vocabulary.
+    """
+    if category not in self._mapping:
+      if self._freeze:
+        return self.UNK_id
+      self._mapping[category] = len(self._mapping)
+      if self._support_reverse:
+        self._reverse_mapping.append(category)
+    return self._mapping[category]
 
-	def add(self, category, count=1):
-		"""
-		Adds count of the category to the frequency table.
+  def add(self, category, count=1):
+    """
+    Adds count of the category to the frequency table.
 
-		Args:
-			category: string or integer, category to add frequency to.
-			count: optional integer, how many to add.
-		"""
-		category_id = self.get(category)
-		if category_id <= self.UNK_id:
-			return
-		self._freq[category] += count
+    Args:
+      category: string or integer, category to add frequency to.
+      count: optional integer, how many to add.
+    """
+    category_id = self.get(category)
+    if category_id <= self.UNK_id:
+      return
+    self._freq[category] += count
 
-	def trim(self, min_frequency, max_frequency=-1):
-		"""Trims vocabulary for minimum frequency.
-		Remaps ids from 1..n in sort frequency order.
-		where n - number of elements left.
+  def trim(self, min_frequency, max_frequency=-1):
+    """Trims vocabulary for minimum frequency.
+    Remaps ids from 1..n in sort frequency order.
+    where n - number of elements left.
 
-		Args:
-			min_frequency: minimum frequency to keep.
-			max_frequency: optional, maximum frequency to keep.
-				Useful to remove very frequent categories (like stop words).
-		"""
-		# Sort by alphabet then reversed frequency.
-		self._freq = sorted(
-			sorted(
-				six.iteritems(self._freq),
-				key=lambda x: (isinstance(x[0], str), x[0])),
-			key=lambda x: x[1],
-			reverse=True)
+    Args:
+      min_frequency: minimum frequency to keep.
+      max_frequency: optional, maximum frequency to keep.
+        Useful to remove very frequent categories (like stop words).
+    """
+    # Sort by alphabet then reversed frequency.
+    self._freq = sorted(
+      sorted(
+        six.iteritems(self._freq),
+        key=lambda x: (isinstance(x[0], str), x[0])),
+      key=lambda x: x[1],
+      reverse=True)
 
-		self._mapping = {self._unknown_token: self.UNK_id}
-		if self._support_reverse:
-			self._reverse_mapping = [self._unknown_token]
+    self._mapping = {self._unknown_token: self.UNK_id}
+    if self._support_reverse:
+      self._reverse_mapping = [self._unknown_token]
 
-		idx = self._mapping.__len__()
-		for category, count in self._freq:
-			if max_frequency > 0 and count >= max_frequency:
-				continue
-			if count <= min_frequency:
-				break
-			self._mapping[category] = idx
-			idx += 1
-			if self._support_reverse:
-				self._reverse_mapping.append(category)
-		self._freq = dict(self._freq[:idx - 1])  # TODO figure out why add this
+    idx = self._mapping.__len__()
+    for category, count in self._freq:
+      if max_frequency > 0 and count >= max_frequency:
+        continue
+      if count <= min_frequency:
+        break
+      self._mapping[category] = idx
+      idx += 1
+      if self._support_reverse:
+        self._reverse_mapping.append(category)
+    self._freq = dict(self._freq[:idx - 1])  # TODO figure out why add this
 
-	def reverse(self, class_id):
-		"""Given class id reverse to original class name.
-		Args:
-			class_id: Id of the class.
+  def reverse(self, class_id):
+    """Given class id reverse to original class name.
+    Args:
+      class_id: Id of the class.
 
-		Returns:
-			Class name.
+    Returns:
+      Class name.
 
-		Raises:
-			ValueError: if this vocabulary wasn't initialized with support_reverse.
-		"""
-		if not self._support_reverse:
-			raise ValueError("This vocabulary wasn't initialized with support_reverse to support reverse() function.")
-		return self._reverse_mapping[class_id]
+    Raises:
+      ValueError: if this vocabulary wasn't initialized with support_reverse.
+    """
+    if not self._support_reverse:
+      raise ValueError("This vocabulary wasn't initialized with support_reverse to support reverse() function.")
+    return self._reverse_mapping[class_id]
--- nlp_vocab/VocabularyProcessor.py
+++ nlp_vocab/VocabularyProcessor.py
@@ -15,7 +15,6 @@
 # ==============================================================================
 """
 Tokenizer for processing the corpus
-
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -26,172 +25,173 @@ import numpy as np
 from nlp_vocab.Vocab import Vocabulary
 
 try:
-	import cPickle as pickle
+  import cPickle as pickle
 except:
-	import pickle
+  import pickle
 
 TOKENIZER_RE = re.compile(r"[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+",
-													re.UNICODE)
+                          re.UNICODE)
 
 
 def tokenizer(iterator):
-	"""Tokenizer generator.
-		Args:
-			iterator: Input iterator with strings.
+  """Tokenizer generator.
+    Args:
+      iterator: Input iterator with strings.
 
-		Yields:
-			array of tokens per each value in the input.
-	"""
-	for value in iterator:
-		yield TOKENIZER_RE.findall(value.lower())
+    Yields:
+      array of tokens per each value in the input.
+  """
+  for value in iterator:
+    yield TOKENIZER_RE.findall(value.lower())
 
 
 class VocabularyProcessor(object):
-	"""
-	VocabularyProcessor helps build Vocabulary given the corpus to be fitted.
-	If documents are longer, they will be trimmed, if shorter - padded.
-	default special tokens for the vocabulary are __UNK__ and __PAD__.
-	"""
+  """
+  VocabularyProcessor helps build Vocabulary given the corpus to be fitted.
+  If documents are longer, they will be trimmed, if shorter - padded.
+  default special tokens for the vocabulary are __UNK__ and __PAD__.
+  """
 
-	def __init__(self,
-							 max_document_length,
-							 min_frequency=0,
-							 vocabulary=None,
-							 tokenizer_fn=None):
-		"""Initializes a VocabularyProcessor instance.
-		Args:
-			max_document_length: Maximum length of documents.
-			if documents are longer, they will be trimmed, if shorter - padded.
-			min_frequency: Minimum frequency of words in the vocabulary.
-			vocabulary: CategoricalVocabulary object.
-		Attributes:
-			vocabulary_: Vocabulary object.
-		"""
-		self.max_document_length = max_document_length
-		self.min_frequency = min_frequency
-		if vocabulary:
-			self.vocabulary_ = vocabulary
-		else:
-			self.vocabulary_ = Vocabulary()
-		if tokenizer_fn:
-			self._tokenizer = tokenizer_fn
-		else:
-			self._tokenizer = tokenizer
+  def __init__(self,
+               max_document_length,
+               min_frequency=0,
+               vocabulary=None,
+               tokenizer_fn=None):
+    """Initializes a VocabularyProcessor instance.
+    Args:
+      max_document_length: Maximum length of documents.
+      if documents are longer, they will be trimmed, if shorter - padded.
+      min_frequency: Minimum frequency of words in the vocabulary.
+      vocabulary: CategoricalVocabulary object.
+    Attributes:
+      vocabulary_: Vocabulary object.
+    """
+    self.max_document_length = max_document_length
+    self.min_frequency = min_frequency
+    if vocabulary:
+      self.vocabulary_ = vocabulary
+    else:
+      self.vocabulary_ = Vocabulary()
+    if tokenizer_fn:
+      self._tokenizer = tokenizer_fn
+    else:
+      self._tokenizer = tokenizer
 
-	def fit(self, raw_documents):
-		"""Learn a vocabulary dictionary of all tokens in the raw documents.
+  def fit(self, raw_documents):
+    """Learn a vocabulary dictionary of all tokens in the raw documents.
 
-		Args:
-			raw_documents: An iterable which yield either str or unicode.
+    Args:
+      raw_documents: An iterable which yield either str or unicode.
 
-		Returns:
-			self
-		"""
-		for tokens in self._tokenizer(raw_documents):
-			for token in tokens:
-				self.vocabulary_.add(token)
-		if self.min_frequency > 0:
-			self.vocabulary_.trim(self.min_frequency)
-		self.vocabulary_.freeze()
-		return self
+    Returns:
+      self
+    """
+    for tokens in self._tokenizer(raw_documents):
+      for token in tokens:
+        self.vocabulary_.add(token)
+    if self.min_frequency > 0:
+      self.vocabulary_.trim(self.min_frequency)
+    self.vocabulary_.freeze()
+    return self
 
-	def fit_transform(self, raw_documents):
-		"""Learn the vocabulary dictionary and return indexies of words.
-			Args:
-				raw_documents: An iterable which yield either str or unicode.
+  def fit_transform(self, raw_documents):
+    """Learn the vocabulary dictionary and return indexies of words.
+      Args:
+        raw_documents: An iterable which yield either str or unicode.
 
-			Returns:
-				x: iterable, [n_samples, max_document_length]. Word-id matrix.
-		"""
-		self.fit(raw_documents)
-		return self.transform(raw_documents)
+      Returns:
+        x: iterable, [n_samples, max_document_length]. Word-id matrix.
+    """
+    self.fit(raw_documents)
+    return self.transform(raw_documents)
 
-	def transform(self, raw_documents):
-		"""Transform documents to word-id matrix.
-			Convert words to ids with vocabulary fitted with fit or the one
-			provided in the constructor.
+  def transform(self, raw_documents):
+    """Transform documents to word-id matrix.
+      Convert words to ids with vocabulary fitted with fit or the one
+      provided in the constructor.
 
-			Args:
-				raw_documents: An iterable which yield either str or unicode.
+      Args:
+        raw_documents: An iterable which yield either str or unicode.
 
-			Yields:
-				x: iterable, [n_samples, max_document_length]. Word-id matrix.
-		"""
-		for tokens in self._tokenizer(raw_documents):
-			word_ids = np.ones(self.max_document_length, np.int64)  # default addtional missing slot use pad
-			for idx, token in enumerate(tokens):
-				if idx >= self.max_document_length:
-					break  # cut exceeding part, this means user,given their special tokens, need to add their own
-				# special tokens by themself before
-				word_ids[idx] = self.vocabulary_.get(token)
-			yield word_ids
+      Yields:
+        x: iterable, [n_samples, max_document_length]. Word-id matrix.
+    """
+    for tokens in self._tokenizer(raw_documents):
+      word_ids = np.ones(self.max_document_length, np.int64)  # default addtional missing slot use pad
+      for idx, token in enumerate(tokens):
+        if idx >= self.max_document_length:
+          break  # cut exceeding part, this means user,given their special tokens, need to add their own
+        # special tokens by themself before
+        word_ids[idx] = self.vocabulary_.get(token)
+      yield word_ids
 
-	def reverse(self, documents):
-		"""Reverses output of vocabulary mapping to words.
+  def reverse(self, documents):
+    """Reverses output of vocabulary mapping to words.
 
-			Args:
-				documents: iterable, list of class ids.
+      Args:
+        documents: iterable, list of class ids.
 
-			Yields:
-				Iterator over mapped in words documents.
-		"""
-		for item in documents:
-			output = []
-			for class_id in item:
-				output.append(self.vocabulary_.reverse(class_id))
-			yield ' '.join(output)
+      Yields:
+        Iterator over mapped in words documents.
+    """
+    for item in documents:
+      output = []
+      for class_id in item:
+        output.append(self.vocabulary_.reverse(class_id))
+      yield ' '.join(output)
 
-	def prepare_embedding_matrix(self, embedding_filepath):
-		"""
-		after fit the doc, prepare the corresponding embedding
-		matrix of which the index is consistent with word index.
-		:param embedding_filepath|str: embedding file path, embedding could be either binary or text
-		:return: embedding_matrix|np.array, embedding_matrix which vector index is consistent
-																with word index in vocab after trim
-		"""
-		def check_emb_dim():
-			with open(embedding_filepath, 'r') as fin:  # TODO binary load
-				for i, value in enumerate(fin):
-					if i == 0: continue  # might be header
-					values = np.asarray(value.split()[1:], dtype='float32')
-					return values.shape[0]
+  def prepare_embedding_matrix(self, embedding_filepath):
+    """
+    after fit the doc, prepare the corresponding embedding
+    matrix of which the index is consistent with word index.
+    :param embedding_filepath|str: embedding file path, embedding could be either binary or text
+    :return: embedding_matrix|np.array, embedding_matrix which vector index is consistent
+                                with word index in vocab after trim
+    """
 
-		assert self.vocabulary_.get_freeze_state() == True, \
-			"vocabulary should be freezed before prepare embedding"
+    def check_emb_dim():
+      with open(embedding_filepath, 'r') as fin:  # TODO binary load
+        for i, value in enumerate(fin):
+          if i == 0: continue  # might be header
+          values = np.asarray(value.split()[1:], dtype='float32')
+          return values.shape[0]
 
-		num_words = self.vocabulary_.__len__()
-		embedding_dim = check_emb_dim()
-		embedding_matrix = np.zeros((num_words, embedding_dim))
+    assert self.vocabulary_.get_freeze_state() == True, \
+      "vocabulary should be freezed before prepare embedding"
 
-		# load and add necessary embedding
-		with open(embedding_filepath, 'r') as f:  # TODO binary load
-			for line in f:
-				values = line.split()
-				word = values[:len(values)-embedding_dim]
-				word = " ".join(word)
-				word_index = self.vocabulary_.get(word)
-				if word_index != self.vocabulary_.UNK_id:  # if embedding words in vocab, load
-					vector = np.asarray(values[-embedding_dim:], dtype='float32')
-					embedding_matrix[word_index] = vector
+    num_words = self.vocabulary_.__len__()
+    embedding_dim = check_emb_dim()
+    embedding_matrix = np.zeros((num_words, embedding_dim))
 
-		return embedding_matrix
+    # load and add necessary embedding
+    with open(embedding_filepath, 'r') as f:  # TODO binary load
+      for line in f:
+        values = line.split()
+        word = values[:len(values) - embedding_dim]
+        word = " ".join(word)
+        word_index = self.vocabulary_.get(word)
+        if word_index != self.vocabulary_.UNK_id:  # if embedding words in vocab, load
+          vector = np.asarray(values[-embedding_dim:], dtype='float32')
+          embedding_matrix[word_index] = vector
 
-	def save(self, filename):
-		"""Saves vocabulary processor into given file.
-			Args:
-			filename: Path to output file.
-		"""
-		with open(filename, 'wb') as f:
-			f.write(pickle.dumps(self))
+    return embedding_matrix
 
-	@classmethod
-	def restore(cls, filename):
-		"""Restores vocabulary processor from given file.
-			Args:
-				filename: Path to file to load from.
+  def save(self, filename):
+    """Saves vocabulary processor into given file.
+      Args:
+      filename: Path to output file.
+    """
+    with open(filename, 'wb') as f:
+      f.write(pickle.dumps(self))
 
-			Returns:
-				VocabularyProcessor object.
-		"""
-		with open(filename, 'rb') as f:
-			return pickle.loads(f.read())
+  @classmethod
+  def restore(cls, filename):
+    """Restores vocabulary processor from given file.
+      Args:
+        filename: Path to file to load from.
+
+      Returns:
+        VocabularyProcessor object.
+    """
+    with open(filename, 'rb') as f:
+      return pickle.loads(f.read())
